234ef1926b6e773d493a02d0133c33587e5f1704
mvinchankar <mvinchankar18@gmail.com>
2019-11-11 10:15:58 +0530

    Added Part Time Feature

--- empFinal.sh
+++ empFinal.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -x
-isPresent=$((RANDOM%2))
-if [ ${isPresent} -eq 1 ]
+employeeCheck=$((RANDOM%3))
+isFullTime=1;
+isPartTime=2;
+if [ $employeeCheck -eq $isFullTime ]
 then
-	echo "Employee is Present";
+	
+	echo "Employee is Full Time";
+
+elif [ $employeeCheck -eq $isPartTime ]
+then
+	echo "Employee is Part Time";
 else
-	echo"Employee is Absent";
+	echo"Employee is Part Time";
 fi
+
